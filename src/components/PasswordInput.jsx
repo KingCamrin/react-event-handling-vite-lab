79071fb031482ca9ef80354954022f67a203bd1f
Camrin <camrindavis58@gmail.com>
2025-11-08 10:40:38 -0600

    Added Sumbmit button and password input

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,9 +1,17 @@
 // Code PasswordInput Component Here
+import React from "react";
 
 function PasswordInput (){
+    function handleChange(event){
+        console.log("Entering password…");
+    }
+    
     return(
-        <></>
+        <input type="password" onChange={handleChange} />
     )
 }
 
+
 export default PasswordInput;
+
+
